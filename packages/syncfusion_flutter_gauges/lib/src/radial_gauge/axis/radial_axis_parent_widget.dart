5510fb7389a9361546f403f666cb662533794aae
Mirza Moin <38783381+MirzaBirds@users.noreply.github.com>
2023-08-20 18:55:03 +0530

    Solved constructor error
    Solved Error: A constant constructor can't call a non-constant super constructor.
          : super(key: key, children: children);

--- packages/syncfusion_flutter_gauges/lib/src/radial_gauge/axis/radial_axis_parent_widget.dart
+++ packages/syncfusion_flutter_gauges/lib/src/radial_gauge/axis/radial_axis_parent_widget.dart
@@ -32,7 +32,7 @@ const double kDefaultRadialGaugeSize = 350.0;
 /// Represents the renderer of radial gauge axis.
 class RadialAxisParentWidget extends MultiChildRenderObjectWidget {
   /// Creates instance for [RadialGaugeRenderWidget].
-  const RadialAxisParentWidget({Key? key, required List<Widget> children})
+   RadialAxisParentWidget({Key? key, required List<Widget> children})
       : super(key: key, children: children);
 
   @override
